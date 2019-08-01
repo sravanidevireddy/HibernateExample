487f293b05be4bae9a6bde1801e18f522a39bf27
798019 <798019@CTSAUS-LT-491.cts.com>
2019-08-01 11:13:01 +1000

    added comment

--- src/main/java/academy/ausgrads/example/hibernate/SessionPersistExample.java
+++ src/main/java/academy/ausgrads/example/hibernate/SessionPersistExample.java
@@ -6,30 +6,30 @@ import org.hibernate.Transaction;
 import academy.ausgrads.example.entity.Name;
 
 public class SessionPersistExample {
-  public static void main(String[] args) {
-    Session session = null;
-    Transaction transaction = null;
-    try {
-      session = HibernateUtil.getSessionFactory().openSession();
-      transaction = session.beginTransaction();
+	public static void main(String[] args) {
+		Session session = null;
+		Transaction transaction = null;
+		try {
+			session = HibernateUtil.getSessionFactory().openSession();
+			transaction = session.beginTransaction();
 
-      Name name = new Name();
-      name.setId(2l);
-      name.setName("Alice");
-      session.persist(name);
+			Name name = new Name();
+			name.setId(2l);
+			name.setName("Devireddy");// set the name
+			session.persist(name);
 
-      transaction.commit();
-    } catch (Exception e) {
-      if (transaction != null) {
-        transaction.rollback();
-      }
-      e.printStackTrace();
-    } finally {
-      if (session != null) {
-        session.close();
-      }
-    }
+			transaction.commit();
+		} catch (Exception e) {
+			if (transaction != null) {
+				transaction.rollback();
+			}
+			e.printStackTrace();
+		} finally {
+			if (session != null) {
+				session.close();
+			}
+		}
 
-    HibernateUtil.shutdown();
-  }
+		HibernateUtil.shutdown();
+	}
 }
--- src/main/java/academy/ausgrads/example/hibernate/SessionSaveExample.java
+++ src/main/java/academy/ausgrads/example/hibernate/SessionSaveExample.java
@@ -6,34 +6,34 @@ import org.hibernate.Transaction;
 import academy.ausgrads.example.entity.Name;
 
 public class SessionSaveExample {
-  public static void main(String[] args) {
-    Session session = null;
-    Transaction transaction = null;
+	public static void main(String[] args) {
+		Session session = null;
+		Transaction transaction = null;
 
-    try {
+		try {
 
-      session = HibernateUtil.getSessionFactory().openSession();
-      transaction = session.beginTransaction();
-      //transaction.begin();
+			session = HibernateUtil.getSessionFactory().openSession();
+			transaction = session.beginTransaction();
+			// transaction.begin();
 
-      Name name = new Name();
-      name.setId(1l);
-      name.setName("Peter");
-      session.save(name);
+			Name name = new Name();
+			name.setId(1l);
+			name.setName("Sravani");
+			session.save(name);
 
-      transaction.commit();
+			transaction.commit();
 
-    } catch (Exception e) {
-      e.printStackTrace();
-      if (transaction != null) {
-        transaction.rollback();
-      }
-    } finally {
-      if (session != null) {
-        session.close();
-      }
-    }
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (transaction != null) {
+				transaction.rollback();
+			}
+		} finally {
+			if (session != null) {
+				session.close();
+			}
+		}
 
-    HibernateUtil.shutdown();
-  }
+		HibernateUtil.shutdown();
+	}
 }
